d37ce002a38b63dbe6abcf7d4602224437a38736
Nikita <nekit.im@yandex.ru>
2022-10-08 16:20:36 +0300

    Start

---
+++ Explorer.Shared.ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace Explorer.Shared.ViewModels;
+
+public class MainViewModel: INotifyPropertyChanged
+{
+    private string _mainDiskName;
+
+    public string MainDiskName { get; set; }
+ 
+    
+        
+    public MainViewModel()
+    {
+        MainDiskName = Environment.SystemDirectory;// Передача системной дириктории 
+            
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
