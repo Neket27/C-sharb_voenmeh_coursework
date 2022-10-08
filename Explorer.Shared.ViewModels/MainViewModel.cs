96904ee25151037636d1a6d0b75d55c5e7e148d9
Nikita <nekit.im@yandex.ru>
2022-10-08 17:34:40 +0300

    add conclusion disks

--- Explorer.Shared.ViewModels/MainViewModel.cs
+++ Explorer.Shared.ViewModels/MainViewModel.cs
@@ -1,28 +1,27 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using JetBrains.Annotations;
 
 namespace Explorer.Shared.ViewModels;
 
-public class MainViewModel: INotifyPropertyChanged
+public class MainViewModel: BaseViewModel
 {
-    private string _mainDiskName;
+    #region publicProperties
 
     public string MainDiskName { get; set; }
- 
-    
-        
+    public string FilePath { get; set; }
+    public ObservableCollection<string> DerectoriesAndFiles { get; set; } = new ObservableCollection<string>();
+
+    #endregion
+
+    #region Constructor
     public MainViewModel()
     {
         MainDiskName = Environment.SystemDirectory;// Передача системной дириктории 
-            
+        foreach (var logicalDrive in Directory.GetLogicalDrives())
+        {
+            DerectoriesAndFiles.Add(logicalDrive);
+        }
     }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    [NotifyPropertyChangedInvocator]
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
+    #endregion
 }
