f9144ded22a2418765010df1d4ed18da4a352566
Nikita <nekit.im@yandex.ru>
2022-10-23 14:03:28 +0300

    add separation image by Directory and File

---
+++ C-sharb_voenmeh_coursework/Convertores/FileEntityToImageConverter.cs
@@ -0,0 +1,42 @@
+﻿using app.models;
+using Explorer.Shared.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace C_sharb_voenmeh_coursework.Convertores
+{
+    internal class FileEntityToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var image = new Image();
+
+            if(value is DirectoryPC)
+            {
+              var resource =  Application.Current.TryFindResource("folder");
+
+                if (resource is Image img)
+                    return img.Source;
+               
+            }
+            else if(value is FilePC)
+            {
+                var resource = Application.Current.TryFindResource("file");
+
+                if (resource is Image img)
+                    return img.Source;
+            }
+         
+            return image;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
